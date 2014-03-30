333b024bfe3e08f5a569c3d0071769b0e04df694
Bill Fienberg <billfienberg@gmail.com>
2014-03-30 16:37:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StudentTracker::Application.config.secret_key_base = 'fd06b7f4af4763b4cd172606399dc7a3ce45184de9ef4677ffa839162c062fed28c9e8bdf4ca624f1632ea414fef9a72665e27245ea5f327445d47eaa00f98d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StudentTracker::Application.config.session_store :cookie_store, key: '_StudentTracker_session'
