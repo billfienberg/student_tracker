79a486b920a3723ce93670d7a3cf9645ac89d880
Bill Fienberg <billfienberg@gmail.com>
2014-03-30 16:59:47 -0500

    Add student model and controller with index action and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 StudentTracker::Application.routes.draw do
+  root "students#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
