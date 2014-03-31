1b2f1a34031c4cd7a76452fa127d0e58d1216c0e
Todd Wacker <twac05@yahoo.com>
2014-03-30 20:01:51 -0500

    Added cohort instance variables to Students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,5 +2,11 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.all
+    @cohort1 = Student.where(cohort_number: 1)
+    @cohort2 = Student.where(cohort_number: 2)
+    @cohort3 = Student.where(cohort_number: 3)
+    @cohort4 = Student.where(cohort_number: 4)
+    @cohort5 = Student.where(cohort_number: 5)
+    @austin_grads = Student.where(current_city: "Austin")
   end
 end
